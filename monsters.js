e724f63033dd702205aa77bf9ce2d4331fb9f157
Ld2000king <liavdimri12@gmail.com>
2026-08-15 16:03:19 +0300

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -566,7 +566,7 @@ const CARD_POOL = [
       img: 'art/web-weaver.jpg', imgPos: 'center 18%',
       art: { body: 'tall', c1: '#151B2E', c2: '#4FA8FF', eyes: 2, eyeStyle: 'glow', horns: 'none', mouth: 'none', extra: 'aura', front: 'plates' } },
 
-    { id: 39, name: 'מייק העכביש המוזהב', power: 7, ability: 'weaken', el: 'red',
+    { id: 39, name: 'מייק העכביש המוזהב', power: 7, ability: 'nullify', el: 'red',
       img: 'art/web-guardian.jpg', imgPos: 'center 12%',
       art: { body: 'tall', c1: '#7A1420', c2: '#F0C24A', eyes: 2, eyeStyle: 'glow', horns: 'none', mouth: 'grin', extra: 'tentacles', front: 'plates' } },
 
